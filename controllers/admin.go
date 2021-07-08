799e210c3034cfae2f0c28e2532439ff437f84eb
PBK-B <pbk-b@PBK6.cn>
2021-07-08 14:27:56 +0800

    fix: 修复登陆时候多次重定向问题

--- controllers/admin.go
+++ controllers/admin.go
@@ -1,16 +1,25 @@
 package controllers
 
 import (
+	"fmt"
+
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func (c *AdminController) Get() {
 	_, _, is := isLogin(c)
 
+	flash := beego.NewFlash()
+	if _, ok := flash.Data["notice"]; ok {
+		c.TplName = "admin.tpl"
+		return
+	}
+
 	if !is {
 		// token 获取失败或失效，或用户被禁用将跳转登陆
 		c.Redirect("/login", 301)
 		c.Finish()
+		return
 	}
 
 	c.TplName = "admin.tpl"
@@ -22,10 +31,17 @@ type AdminController struct {
 }
 
 func (c *AdminController) LoginPage() {
-	_, _, is := isLogin(c)
+	token, _, is := isLogin(c)
 	if is {
+
+		flash := beego.NewFlash()
+		flash.Notice(token)
+		flash.Store(&c.Controller)
+
 		c.Redirect("/admin", 301)
-	} else {
-		c.TplName = "login.tpl"
+		return
 	}
+
+	c.TplName = "login.tpl"
+
 }
